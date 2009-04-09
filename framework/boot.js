c48fdb201d615cef3da3e045c211edb3cdd2e296
Jan Varwig <jan@varwig.org>
2009-04-09 18:15:22 +0200

    Jack Example copied to boot.js

--- framework/boot.js
+++ framework/boot.js
@@ -1,40 +1,97 @@
-var KUPO_ROOT = "../"
-$LOAD_PATH = KUPO_ROOT + "framework/lib" + ":" + KUPO_ROOT + "vendor/narwhal/lib" + ":" + KUPO_ROOT + "vendor/v8cgi";
-
-//V8
-// include(KUPO_ROOT + 'vendor/v8cgi/js.js')
-V8CGI = {}
-// V8CGI.HTML    = require(KUPO_ROOT + 'vendor/v8cgi/html.js').HTML
-// V8CGI.HTTP    = require(KUPO_ROOT + 'vendor/v8cgi/http.js').HTTP
-// V8CGI.Session = require(KUPO_ROOT + 'vendor/v8cgi/session.js').Session
-V8CGI.Util    = require(KUPO_ROOT + 'vendor/v8cgi/util.js').Util
-
-// Print
-global.print = function(x) {
-  System.stdout(x + "\n")
-}
-
-// Debugging
-// $DEBUG = true;
-// print("Content-Type: text/plain\n\n")
-// global.pd = function(x){
-//   print(d(x))
-// }
-global.d = function(x){
-  V8CGI.Util.serialize(x, true)
-}
-
-
-// Load Jack and Narwhal
-include(KUPO_ROOT + "vendor/narwhal/narwhal.js");
-
 var Jack = require("jack");
 
-var app = function(env) {
-    var Request = require('jack/request').Request;
-    var r = new Request(env)
-    return [200, {"Content-Type":"text/plain"}, "hello world! How are you?" + r.scriptName()];
-}
-// app = Jack.Lint(app);
+var map = {};
 
-Jack.Handler.V8CGI.run(app, request, response);
+// an extremely simple Jack application
+map["/hello"] = function(env) {
+    return [200, {"Content-Type":"text/plain"}, "Hello from " + env["SCRIPT_NAME"]];
+}
+
+// 1/6th the time this app will throw an exception
+map["/httproulette"] = function(env) {
+    // if you have the ShowExceptions middleware in the pipeline it will print the error.
+    // otherwise the server/handler will print something
+    if (Math.random() > 5/6)
+        throw new Error("bam!");
+    
+    return [200, {"Content-Type":"text/html"}, 'whew!<br /><a href="httproulette">try again</a>'];
+}
+
+var form = '<form action="" method="get"><input type="text" name="name" value="" id="some_name"><input type="submit" value="go"></p></form>';
+
+// an index page demonstrating using a Response object
+map["/"] = function(env) {
+    var request = new Jack.Request(env),
+        response = new Jack.Response();
+
+    response.write('hello ' + (request.GET("name") || form) +"<br />");
+        
+    response.write('<a href="hello">hello</a><br />');
+    response.write('<a href="httproulette">httproulette</a><br />');
+    response.write('<a href="narwhal">narwhal</a><br />');
+    response.write('<a href="stream">stream</a><br />');
+    response.write('<a href="stream1">stream1</a><br />');
+    response.write('<a href="cookie">cookie</a><br />');
+
+    return response.finish();
+}
+
+map["/narwhal"] = Jack.Narwhal;
+
+// use the JSONP middleware on this one
+map["/jsontest"] = Jack.JSONP(function(env) {
+    return [200, { "Content-Type" : "application/json" }, "{ hello : 'world' }"];
+});
+
+map["/files"] = Jack.File(".");
+
+map["/stream"] = function(env) {
+    return [200,
+        {"Content-Type":"text/html", "Transfer-Encoding":"chunked"},
+        { forEach : function(write) {
+            for (var i = 0; i < 50; i++) { 
+                java.lang.Thread.currentThread().sleep(100); 
+                write("hellohellohellohellohellohellohellohellohellohellohellohellohello<br />"); 
+            }
+        }
+    }];
+}
+
+
+map["/stream1"] = function(env) {
+    var res = new Jack.Response(200, {"Transfer-Encoding":"chunked"});
+    return res.finish(function(response) {
+        for (var i = 0; i < 50; i++) { 
+            java.lang.Thread.currentThread().sleep(100); 
+            response.write("hellohellohellohellohellohellohellohellohellohellohellohellohello<br />"); 
+        }
+    });
+}
+
+map["/cookie"] = function(env) {
+    var request = new Jack.Request(env),
+        response = new Jack.Response();
+        
+    var name = request.POST("name");
+    
+    if (typeof name === "string") {
+        response.write("setting name: " + name + "<br />");
+        response.setCookie("name", name);
+    }
+    
+    var cookies = request.cookies();
+    if (cookies["name"])
+        response.write("previously saved name: " + cookies["name"] +"<br />")
+        
+    response.write('<form action="cookie" method="post" enctype="multipart/form-data"><input type="text" name="name" value="" id="some_name"><input type="submit" value="go"></p></form>')
+    
+    return response.finish();
+}
+
+// middleware:
+
+// apply the URLMap
+var app = Jack.ContentLength(Jack.URLMap(map));
+
+// serve the "/example" directory files
+Jack.Static(app, { urls : ["/example"] });
