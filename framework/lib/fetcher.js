b7fcc3570d6cf3d11ad83936a44415a80f298c47
Jan Varwig <jan@varwig.org>
2009-04-22 13:02:58 +0200

    Added some comments to the Fetcher

--- framework/lib/fetcher.js
+++ framework/lib/fetcher.js
@@ -1,9 +1,11 @@
 var Fetcher = exports.Fetcher = {}
 
+/* Generate an absolute filename from the type and the name of a file to be loaded */
 var filename = function(name, type) {
   return $KUPO_HOME + '/app/' + type + "/" + name + ".js"
 }
 
+/* Return a factory function for the contents of a file */
 var evaluate = function (text, filename) {
     if (typeof Packages !== "undefined" && Packages.java)
         return Packages.org.mozilla.javascript.Context.getCurrentContext().compileFunction(
@@ -17,6 +19,11 @@ var evaluate = function (text, filename) {
         return new Function("require", "exports", "environment", text);
 };
 
+/* Determine the Name of the variable containing the loaded object in the
+ * exports hash of the factory
+ * 
+ * Example: varName('foo', 'controller') = 'FooController'
+ */
 var varName = function(name, type) {
   var head = name.charAt(0).toUpperCase()
   var tail = name.slice(1)
@@ -25,6 +32,7 @@ var varName = function(name, type) {
   return name
 }
 
+/* Returns a Model or Controller  */
 var fetch = function(name, type) {
   var fname      = filename(name, type);
   var content    = readFile(fname)
@@ -34,11 +42,12 @@ var fetch = function(name, type) {
   return e[varName(name, type)]
 }
 
-
+/* Return a controller by a given name */
 Fetcher.fetchController = function(name) {
   return fetch(name, 'controller');
 }
 
+/* Return a model by a given name */
 Fetcher.fetchModel = function(name) {
   return fetch(name, 'model');  
 }
