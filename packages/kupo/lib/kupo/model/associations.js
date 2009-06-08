41e09921c6f87c8a2c727bfbd44156007dcd6e6d
Jan Varwig <jan@varwig.org>
2009-06-08 15:07:58 +0200

    Associations moved into subdirectory

--- packages/kupo/lib/kupo/model/associations.js
+++ packages/kupo/lib/kupo/model/associations.js
@@ -1,6 +1,6 @@
 var Associations = exports.Associations = {};
 
-Associations.hasOne        = require('kupo/model/has_one'        ).hasOne;
-Associations.hasMany       = require('kupo/model/has_many'       ).hasMany;
-Associations.belongsTo     = require('kupo/model/belongs_to'     ).belongsTo;
-Associations.belongsToMany = require('kupo/model/belongs_to_many').belongsToMany;
+Associations.hasOne        = require('kupo/model/associations/has_one'        ).hasOne;
+Associations.hasMany       = require('kupo/model/associations/has_many'       ).hasMany;
+Associations.belongsTo     = require('kupo/model/associations/belongs_to'     ).belongsTo;
+Associations.belongsToMany = require('kupo/model/associations/belongs_to_many').belongsToMany;
