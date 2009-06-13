0748198860cfaa97e7a1d94e2b60dad8a608d049
Jan Varwig <jan@varwig.org>
2009-06-13 15:24:08 +0200

    First Version of the SplitLoader

--- jackconfig.js
+++ jackconfig.js
@@ -6,6 +6,13 @@ global.pp = function (x) {
   print(require('test/jsdump').jsDump.parse(x))
 }
 
+var serverLoader = require('sandbox').Loader({
+  paths      : require.loader.loader.paths,
+  extensions : ['','.server.js']
+})
+require.loader.loaders.push(['.server.js', serverLoader]);
+require.loader.loaders.reverse();
+
 //Require the Kupo Loader
 var Dispatcher = require('kupo/dispatcher').Dispatcher
 
--- packages/kupo/tests/main.js
+++ packages/kupo/tests/main.js
@@ -3,6 +3,14 @@ global.pp = function (x) {
   print(require('test/jsdump').jsDump.parse(x))
 }
 
+var serverLoader = require('sandbox').Loader({
+  paths      : require.loader.loader.paths,
+  extensions : ['','.server.js']
+})
+require.loader.loaders.push(['.server.js', serverLoader]);
+require.loader.loaders.reverse();
+
+
 exports.testMongoAdapter = require("./mongo_adapter-tests");
 exports.testMongoConverter = require("./mongo-converter-tests");
 exports.testModel = require("./model-tests");
